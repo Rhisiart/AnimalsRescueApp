b7a6cbc93934652facd0bb8c94f7a33d2691e470
Rhisiart <ricardobenfiquista13@gmail.com>
2023-09-22 17:19:48 +0100

    get current location

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,4 +1,5 @@
 import { styles } from "@/styles/styles";
+import { Accuracy, LocationObject, LocationOptions, getCurrentPositionAsync, } from "expo-location";
 import React from "react";
 import { View } from "react-native";
 import MapView from "react-native-maps";
@@ -8,14 +9,42 @@ interface IProps {
 }
 
 const Map : React.FC<IProps> = () => {
-  
-  
+  const [currentLocation, setCurrentLocation] = React.useState<LocationObject>();
+
+  const getCurrentLocation = async () => {
+    try {
+      const options : LocationOptions = {
+        accuracy: Accuracy.Highest,
+        mayShowUserSettingsDialog: true
+      }
+      const locationObj = await getCurrentPositionAsync(options);
+
+      setCurrentLocation(locationObj);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  React.useEffect(() => {
+    getCurrentLocation();
+  }, [])
 
   return (
     <View style={styles.container}>
+    {
+      currentLocation &&
       <MapView
         style={styles.map}
-      />
+        initialRegion={{
+          latitude: currentLocation.coords.latitude,
+          longitude: currentLocation.coords.longitude,
+          latitudeDelta : 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      >
+      </MapView>
+    }
     </View>
   )
 }
